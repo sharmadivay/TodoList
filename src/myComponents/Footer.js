891088bf9dee4061348fed0be3e2763c61e4e306
DiVaY ShArMa <115571620+sharmadivay@users.noreply.github.com>
2023-10-01 15:48:02 +0530

    footer

--- src/myComponents/Footer.js
+++ src/myComponents/Footer.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import "../App.css"
 const Footer = () => {
-  let footerStyle ={
-    position: "relative",
-    width: "100%",
-    bottom: 0,
-  }
+  // let footerStyle ={
+  //   position: "relative",
+  //   width: "100%",
+  //   bottom:0,
+  //   left: 0,
+    
+  // }
   return (
-    <footer className='bg-dark  text-light py-3 footer' style={footerStyle}>
+    <footer className='bg-dark  text-light py-3 footer' >
       <p className="text-center">
         Copyright &copy; MyTodoList.com
       </p>
